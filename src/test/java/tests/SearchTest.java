0f968789f10087c229b9b0360e678ce2190568e3
andreddd799 <36064944+andreddd799@users.noreply.github.com>
2020-12-04 15:10:39 +0300

    added Yandex search test

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,8 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -13,6 +13,14 @@ public class SearchTest {
     void searchGoogle() {
         open("https://www.google.com/");
         $(byName("q")).setValue("Selenide").pressEnter();
-        $("html").shouldHave(Condition.text("selenide.org"));
+        $("html").shouldHave(text("selenide.org"));
+    }
+
+    @Test
+    void searchYandex() {
+        open("https://www.yandex.ru/");
+        $("#text").setValue("Selenide").pressEnter();
+        $(".serp-list").$$(".serp-item").shouldHaveSize(11);
+        $(".serp-list").$$(".serp-item").get(0).shouldHave(text("Selenide"));
     }
 }
